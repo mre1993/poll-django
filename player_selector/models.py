ff99475f9d4922a843a8b7b5194fb2c982d5a741
mre1993 <139882080+mre1993@users.noreply.github.com>
2025-05-10 14:59:16 +0330

    Update models.py

--- player_selector/models.py
+++ player_selector/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from accounts.models import User
+from django.contrib.auth.models import User
 
 class Place(models.Model):
     name = models.CharField(max_length=255)
