ded13f2980267522dba7636aad53a52ed914c5f4
mre1993 <139882080+mre1993@users.noreply.github.com>
2025-05-10 14:35:08 +0330

    Update views.py

--- player_selector/views.py
+++ player_selector/views.py
@@ -5,6 +5,39 @@ from .forms import VoteForm, ParentSelectionForm, ChildSelectionForm
 from django.db.models import Count, F
 from collections import defaultdict
 from django.http import JsonResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomRegisterForm
+from django.contrib import messages
+from django.contrib.auth import logout
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("player:vote")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("player:vote")
+    else:
+        form = CustomRegisterForm()
+    return render(request, "register.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
 
 @login_required
@@ -13,7 +46,6 @@ def vote(request):
         form = VoteForm(request.POST)
         if form.is_valid():
             player = form.cleaned_data['player']
-            # Check if the user has already voted for this player
             if not Vote.objects.filter(
                 player=player,
                 user=request.user
